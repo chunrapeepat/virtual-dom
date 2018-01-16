bc2a0cfb55cd9316235235e00cea12ae5b91bb8f
chunza2542 <chunza2542@hotmail.com>
2018-01-16 20:25:36 +0700

    create shallow element fro, virtual-dom

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ function diff(newNode, oldNode) {  //@
 
 //// PATCH
 
-function createElement(node) { //@
-
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node)
+  }
+  return document.createElement(node.type)
 }
 
 function setProp(target, name, value) { //@
@@ -49,18 +52,22 @@ function patch(parent, patches, index = 0) {
 
 }
 
-//// My Application
-
 function flatten(arr) {
-
+  return [].concat.apply([], arr)
 }
 
 function h(type, props, ...children) {
-
+  props = props || {}
+  return { type, props, children: flatten(children) }
 }
 
 function view(count) {
-
+  return (
+    <ul id="cool" className="foo">
+      <li>Text 1</li>
+      <li>Text 2</li>
+    </ul>
+  )
 }
 
 function tick(el, count) {
@@ -68,5 +75,6 @@ function tick(el, count) {
 }
 
 function render(el) {
-
+  // create real-dom from virtual-dom and append to element
+  el.appendChild(createElement(view(0)))
 }
